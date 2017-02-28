37b32e93443948afd0507775665aa51c424f8a74
sparder <brxonline@163.com>
2017-02-28 17:32:55 +0800

    hystrix board fix

--- loginservice/src/main/java/com/brx/test/service/feign/UserFeiginService.java
+++ loginservice/src/main/java/com/brx/test/service/feign/UserFeiginService.java
@@ -1,6 +1,7 @@
 package com.brx.test.service.feign;
 
 import com.brx.test.service.model.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface UserFeiginService {
 
     @RequestMapping(value="saveUser",method = RequestMethod.POST)
+    //@HystrixCommand()
     public Object saveUser(@RequestBody User user);
 }
